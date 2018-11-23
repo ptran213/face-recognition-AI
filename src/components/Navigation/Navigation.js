d4aea17e541432630ba144ae4f958928595bf0cf
Ivan Tran <phuongtran1312@gmail.com>
2018-11-23 09:52:21 -0800

    Modifying navigation bar

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Navigation = () => {
     return(
-        <nav>
-            <p>Log In</p>
+        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <p className='f3 link dim black underline pa3 pointer'>Log In</p>
         </nav>
     )
 }
