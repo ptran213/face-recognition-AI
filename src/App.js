9cd6431439bfce329735925f859ff220a48a4783
Ivan Tran <phuongtran1312@gmail.com>
2018-11-24 08:23:47 -0800

    Adding Particles Background

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
@@ -6,16 +7,34 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 
 
+const particleOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+
+    move: {
+      speed: 6
+    }
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/*<FaceRecognition /> */}
-      
+        <Particles  className='particles' 
+                    params={particleOptions} 
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/*<FaceRecognition /> */}
       </div>
     );
   }
